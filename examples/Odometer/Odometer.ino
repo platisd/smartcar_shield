b8f173fc15955efacdacd6792027e8edb9ab9d2a
dimi88 <kurukii88@gmail.com>
2015-02-16 16:15:06 +0100

    small fix

--- examples/Odometer/Odometer.ino
+++ examples/Odometer/Odometer.ino
@@ -5,7 +5,6 @@ unsigned long startTime;
 unsigned int timeOver = 20000; //20 seconds
 const int encoderPin = 19; //digital pin 19 (interrupt 4 on arduino mega)
 
-
 void setup() {
   Serial.begin(9600);
   encoder.attach(encoderPin);
