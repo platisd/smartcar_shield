6bce6dd348ca42350cfe5c39ae157b50bc5e1bc3
dimi88 <kurukii88@gmail.com>
2015-02-17 01:02:48 +0100

    Gyroscope example

---
+++ examples/Gyroscope/Gyroscope.ino
@@ -0,0 +1,19 @@
+#include <Smartcar_sensors.h>
+#include <Smartcar.h>
+#include <Wire.h>
+
+Gyroscope gyro;
+
+void setup() {
+  Serial.begin(9600);
+  gyro.attach();
+  delay(1500);
+  Serial.println("Start measuring!");
+  gyro.begin();
+}
+
+void loop() {
+  Serial.println(gyro.getAngularDisplacement());
+  delay(100);
+}
+
