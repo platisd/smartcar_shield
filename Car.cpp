9170e0ad3e0042637bbabfe61a5fa0b09d397b85
dimi88 <kurukii88@gmail.com>
2015-11-11 01:46:30 +0100

    making the car to avoid corrections when it is just drifting

--- Car.cpp
+++ Car.cpp
@@ -56,18 +56,14 @@ void Car::setSpeed(float newSpeed){
 
 void Car::updateMotors(){
 	if (cruiseControl && (millis() > _lastMotorUpdate + _pidLoopInterval)){
-		float measuredSpeed = getGroundSpeed(); //speed in cm/milliseconds
-		measuredSpeed *= 10; //transform it into m/seconds, so we divide by 100 to turn cm into m and multiply by 1000, to turn ms to sec
-		if (_speed < 0) measuredSpeed *= -1; //if we are going reverse, illustrate that in the value of measuredSpeed
-//		Serial.print("Target: ");
-//		Serial.print(_speed);
-//		Serial.print("\t\tMeasured: ");
-//		Serial.print(measuredSpeed);
-		int controlledSpeed = motorPIDcontrol(_previousControlledSpeed, _speed, measuredSpeed);
-//		Serial.print("\t\tOutput: ");
-//		Serial.println(controlledSpeed);
-		setRawSpeed(controlledSpeed);
-		_previousControlledSpeed = controlledSpeed;
+		if (_speed){ //if _speed is 0, we have already made sure the car is stopped. don't try to adjust if car is just drifting
+			float measuredSpeed = getGroundSpeed(); //speed in cm/milliseconds
+			measuredSpeed *= 10; //transform it into m/seconds, divide by 100 to turn cm into m and multiply by 1000, to turn ms to sec
+			if (_speed < 0) measuredSpeed *= -1; //if we are going reverse, illustrate that in the value of measuredSpeed
+			int controlledSpeed = motorPIDcontrol(_previousControlledSpeed, _speed, measuredSpeed);
+			setRawSpeed(controlledSpeed);
+			_previousControlledSpeed = controlledSpeed;
+		}
 		_lastMotorUpdate = millis();
 	}
 }
