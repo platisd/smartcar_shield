132fcaa53b1585d5b0fdceee570b18c8abf534e1
dimi88 <kurukii88@gmail.com>
2015-11-18 16:27:26 +0100

    added possibility to have inverted controls

--- Car.cpp
+++ Car.cpp
@@ -5,16 +5,11 @@
 /* --- CAR --- */
 
 const unsigned short Car::DEFAULT_PID_LOOP_INTERVAL = 80;
+const unsigned short Car::STANDARD = 1;
+const unsigned short Car::INVERTED = 0; 
 const float Car::DEFAULT_KP = 5.0;
 const float Car::DEFAULT_KI = 0.0;
 const float Car::DEFAULT_KD = 10.0;
-
-const uint8_t MOTOR_LEFT1_PIN = 8;
-const uint8_t MOTOR_LEFT_EN_PIN = 9;
-const uint8_t MOTOR_LEFT2_PIN = 10;
-const uint8_t MOTOR_RIGHT_EN_PIN = 11;
-const uint8_t MOTOR_RIGHT1_PIN = 12;
-const uint8_t MOTOR_RIGHT2_PIN = 13;
 
 const unsigned short BACKWARD = 0;
 const unsigned short FORWARD = 1;
@@ -34,11 +29,26 @@ const int GO_RAW_SPEED = 200;
 const int MAX_RIGHT_STEERING_ANGLE = MAX_RIGHT_DEGREES - STRAIGHT_WHEELS;
 const int MAX_LEFT_STEERING_ANGLE = MAX_LEFT_DEGREES - STRAIGHT_WHEELS;
 
-Car::Car(){
+Car::Car(const unsigned short setup){
 	_cruiseControl = false;
 	_speed = IDLE_RAW_SPEED;
 	_angle = STRAIGHT_WHEELS;
 	_numOfEncoders = 0;
+	if (setup == STANDARD){ //the default motor setup, where right is right
+		MOTOR_LEFT1_PIN = 8;
+		MOTOR_LEFT_EN_PIN = 9;
+		MOTOR_LEFT2_PIN = 10;
+		MOTOR_RIGHT_EN_PIN = 11;
+		MOTOR_RIGHT1_PIN = 12;
+		MOTOR_RIGHT2_PIN = 13;
+	}else{ //the reversed motor setup, where right is left
+		MOTOR_RIGHT1_PIN = 8;
+		MOTOR_RIGHT_EN_PIN = 9;
+		MOTOR_RIGHT2_PIN = 10;
+		MOTOR_LEFT_EN_PIN = 11;
+		MOTOR_LEFT1_PIN = 12;
+		MOTOR_LEFT2_PIN = 13;
+	}
 }
 
 void Car::begin(){
@@ -166,8 +176,8 @@ void Car::setDirection(const unsigned short direction){ //platform specific meth
 }
 
 void Car::setAngle(int angle){ //platform specific method
-	_angle = constrain(STRAIGHT_WHEELS + angle, MAX_LEFT_DEGREES, MAX_RIGHT_DEGREES);
-	setMotors(getSpeed(), constrain(angle, MAX_LEFT_DEGREES - STRAIGHT_WHEELS, MAX_RIGHT_DEGREES - STRAIGHT_WHEELS)); //pass the human-readable value
+	_angle = constrain(angle, MAX_LEFT_DEGREES - STRAIGHT_WHEELS, MAX_RIGHT_DEGREES - STRAIGHT_WHEELS); //constrain the value to the permitted valued the user is allowed to supply
+	setMotors(getSpeed(), getAngle()); //pass the human-readable value that was just supplied
 }
 
 void Car::stop(){ //platform specific method
@@ -192,7 +202,7 @@ float Car::getSpeed(){
 }
 
 int Car::getAngle(){
-	return _angle - STRAIGHT_WHEELS; //return what the user supplied
+	return _angle; //return what the user supplied
 }
 
 void Car::enableCruiseControl(float Kp, float Ki, float Kd, unsigned short pidLoopInterval){
