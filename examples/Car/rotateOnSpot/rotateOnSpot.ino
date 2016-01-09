c4a9d6d7cd9a0f00a06df0373fa32be38af07686
dimi88 <kurukii88@gmail.com>
2016-01-09 15:47:29 +0100

    rotating on spot example

---
+++ examples/Car/rotateOnSpot/rotateOnSpot.ino
@@ -0,0 +1,33 @@
+#include <Smartcar.h>
+#include <Wire.h>
+
+Gyroscope gyro;
+Car car(Car::INVERTED);
+int motorSpeed = 200;
+
+void setup() {
+  gyro.attach();
+  car.begin(gyro);
+  rotateOnSpot(90); //rotate clockwise 90 degrees on spot
+  delay(1000);
+  rotateOnSpot(-90); //rotate counter clockwise 90 degrees on spot
+}
+
+void loop() {
+}
+
+void rotateOnSpot(int targetDegrees){
+  if (!targetDegrees) return; //if the target degrees is 0, don't bother doing anything
+  gyro.begin(); //initiate the measurement
+  /* Let's set opposite speed on each side of the car, so it rotates on spot */
+  if (targetDegrees > 0){ //positive value means we should rotate clockwise
+    car.setMotorSpeed(motorSpeed, -motorSpeed); // left motors spin forward, right motors spin backward
+  }else{ //rotate counter clockwise
+    car.setMotorSpeed(-motorSpeed, motorSpeed); // left motors spin backward, right motors spin forward
+  }
+  while (abs(gyro.getAngularDisplacement()) <= abs(targetDegrees)){ //while we have not reached the desired degrees
+    gyro.update(); //update the gyro readings
+  }
+  car.stop(); //we have reached the target, so stop the car
+}
+
