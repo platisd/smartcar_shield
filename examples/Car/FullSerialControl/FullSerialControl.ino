8bc1979249f0f30161cef2deb80a40676ecd1732
dimi88 <kurukii88@gmail.com>
2016-01-24 23:43:30 +0100

    example that accurately controls the throttle and steering via serial

---
+++ examples/Car/FullSerialControl/FullSerialControl.ino
@@ -0,0 +1,28 @@
+#include <Smartcar.h>
+#include <Wire.h>
+#include <Servo.h>
+
+Car car;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(200);
+  car.begin()
+}
+
+void loop() {
+  handleInput();
+}
+
+void handleInput() { //handle serial input if there is any
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    if (input.startsWith("m")) {
+      int throttle = input.substring(1).toInt();
+      car.setSpeed(throttle);
+    }else if (input.startsWith("t")){
+      int deg = input.substring(1).toInt();
+      car.setAngle(deg);
+    }
+  }
+}
