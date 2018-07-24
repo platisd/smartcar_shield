5ac0093fe7eb4cd0ed92477a94a7af2b3f913a1a
Dimitris Platis <platisd@users.noreply.github.com>
2018-07-24 22:31:46 +0200

    Add coverage reports

---
+++ run_coverage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+caller_dir=$PWD
+# Move to the current script directory
+cd "${0%/*}"
+
+smartcar_test_dir="${TRAVIS_BUILD_DIR}/test"
+coverage_report_dir="${TRAVIS_BUILD_DIR}/coverage-reports"
+
+# Specify which directories are to be excluded from coverage report
+coverage_ignore_dirs="\
+    '/usr/*' \
+"
+
+# Run coverage for ins-node and ins-server
+eval mkdir -p $coverage_report_dir
+eval lcov --directory . --capture --output-file coverage.info
+eval lcov --remove coverage.info $coverage_ignore_dirs --output-file $coverage_report_dir/cpp_coverage.info
+eval lcov --list coverage.info
+
+# Go back to the initial directory when you are done
+cd $caller_dir
