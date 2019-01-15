2d729201fa4db5066a3b7c61331095db075aec5b
Dimitris Platis <platisd@users.noreply.github.com>
2019-01-15 23:41:07 +0100

    Make Smartcar.h as the library's entrypoint
    * Remove old source code (used for reference during refactoring)
    * Remove commented out class declarations (used for reference during refactoring)

--- old_src/UltrasonicSensor.cpp
+++
@@ -1,7 +0,0 @@
-/* Placeholder for an ultrasonic sensor class */
-
-#include "Smartcar.h"
-
-UltrasonicSensor::UltrasonicSensor(){
-    //_sensorMedianDelay = 0; //the default median delay for the ultrasonic sensors will be redefined in child classes if necessary
-}
