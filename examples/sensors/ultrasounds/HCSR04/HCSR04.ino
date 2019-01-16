bc5538ad2eee292d99e0088edfb00a6379042dbd
Dimitris Platis <platisd@users.noreply.github.com>
2019-01-16 21:09:12 +0100

    Update SR04 example to new 5.0 API

--- examples/sensors/ultrasounds/HCSR04/HCSR04.ino
+++ examples/sensors/ultrasounds/HCSR04/HCSR04.ino
@@ -1,12 +1,12 @@
 #include <Smartcar.h>
 
-SR04 front;
 const int TRIGGER_PIN = 6; //D6
 const int ECHO_PIN = 7; //D7
+SR04 front(TRIGGER_PIN, ECHO_PIN, 10);
+
 
 void setup() {
   Serial.begin(9600);
-  front.attach(TRIGGER_PIN, ECHO_PIN); //trigger pin, echo pin
 }
 
 void loop() {
