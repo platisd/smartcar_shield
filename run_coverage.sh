8d7a31345ad938d7d03bd93c9a4f5664d6bd8fe6
Dimitris Platis <platisd@users.noreply.github.com>
2019-06-17 22:39:14 +0200

    Fix coverage report script to run locally

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 caller_dir=$PWD
 
-smartcar_test_dir="${TRAVIS_BUILD_DIR}/test"
-coverage_report_dir="${TRAVIS_BUILD_DIR}/coverage-reports"
+script_dir=$(dirname "$0")
+smartcar_test_dir="${script_dir}/test"
+coverage_report_dir="${script_dir}/coverage-reports"
 
 # Specify which directories are to be excluded from coverage report
 coverage_ignore_dirs="\
     '/usr/*' \
+    '*/test/*' \
+    '*/external/*'\
 "
 
 # Run coverage for ins-node and ins-server
 eval mkdir -p $coverage_report_dir
 eval lcov --directory . --capture --output-file coverage.info
 eval lcov --remove coverage.info $coverage_ignore_dirs --output-file $coverage_report_dir/cpp_coverage.info
-eval lcov --list coverage.info
+eval lcov --list $coverage_report_dir/cpp_coverage.info
 
 # Go back to the initial directory when you are done
 cd $caller_dir
